f6c90afe2a487b855b74cdf24166cdabbef91643
Roffe <joakim@roffe.nu>
2023-10-01 11:40:35 +0200

    pilutta dig

--- release.ps1
+++ release.ps1
@@ -8,6 +8,7 @@ $env:PKG_CONFIG_PATH = "C:\vcpkg\packages\libusb_x86-windows\lib\pkgconfig"
 $env:CGO_CFLAGS = "-IC:\vcpkg\packages\libusb_x86-windows\include\libusb-1.0"
 $env:GOARCH = "386"
 $env:CGO_ENABLED = "1"
+$env:GOEXPERIMENT = "loopvar"
 
 # Execute the command and capture the output
 Write-Host "Building txlogger.exe"
