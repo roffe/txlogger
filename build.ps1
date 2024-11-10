268294e294b91eee3e8e3ec6dd6b0e6368a0922c
Roffe <joakim@roffe.nu>
2024-11-10 18:44:09 +0100

    update plumbing

--- build.ps1
+++ build.ps1
@@ -2,9 +2,9 @@ $env:PKG_CONFIG_PATH = "/vcpkg/packages/libusb_x86-windows/lib/pkgconfig"
 $env:CGO_CFLAGS = "-I/vcpkg/packages/libusb_x86-windows/include/libusb-1.0"
 $env:GOARCH = "386"
 $env:CGO_ENABLED = "1"
-
 $env:CC = "C:\\mingw32\\bin\\gcc.exe"
 $env:CXX = "C:\\mingw32\\bin\\gcc.exe"
+
 Invoke-Expression "rsrc -arch 386 -manifest manifest.xml"
 Invoke-Expression "go generate ./..."
 Invoke-Expression "fyne package -tags combi --release"
