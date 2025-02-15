2aa31359398a3dff918ae07bd64d00cae0adfcd8
Roffe <joakim@roffe.nu>
2025-02-15 12:27:42 +0100

    version

--- main.go
+++ main.go
@@ -152,7 +152,7 @@ func RtlGetVersion() RTL_OSVERSIONINFOEXW {
 func mainDesktop() {
 	ver := RtlGetVersion()
 	log.Println("Windows version", ver.MajorVersion, ver.MinorVersion, ver.BuildNumber)
-	if ver.MajorVersion < 12 {
+	if ver.MajorVersion < 10 {
 		sdialog.Message("txlogger requires Windows 10 or later").Title("Unsupported Windows version").Error()
 		return
 	}
