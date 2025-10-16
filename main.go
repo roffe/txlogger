1435fe8f82b814565fe0d0266d69b622886fa7e3
Roffe <joakim@roffe.nu>
2025-10-16 18:53:25 +0200

    update error message

--- main.go
+++ main.go
@@ -72,7 +72,7 @@ func main() {
 		defer killProcess(p)
 	}
 	if err != nil {
-		debug.Log("cangatewayis not ready: " + err.Error())
+		debug.Log("cangateway is not ready: " + err.Error())
 	}
 
 	tx := app.NewWithID("com.roffe.txlogger")
