3e69c11480ef7b375da56fa1f2912edfd0e951bd
Roffe <joakim@roffe.nu>
2025-10-29 19:21:08 +0100

    remember last preset used per ECU

--- pkg/windows/mainwindow_selects.go
+++ pkg/windows/mainwindow_selects.go
@@ -1,9 +1,12 @@
 package windows
 
 import (
+	"log"
+
 	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/widget"
 	symbol "github.com/roffe/ecusymbol"
+	"github.com/roffe/txlogger/pkg/common"
 	"github.com/roffe/txlogger/pkg/ebus"
 	"github.com/roffe/txlogger/pkg/presets"
 )
@@ -27,26 +30,30 @@ func (mw *MainWindow) createSelects() {
 		}
 	})
 
-	mw.selects.presetSelect = widget.NewSelect(append([]string{"Select preset"}, presets.Names()...), func(s string) {
-		if s == "Select preset" {
+	mw.selects.ecuSelect = widget.NewSelect(common.EcuList, func(s string) {
+		mw.app.Preferences().SetString(prefsSelectedECU, s)
+		idx := symbol.ECUTypeFromString(s)
+		ebus.Publish(ebus.TOPIC_ECU, float64(idx))
+		mw.SetMainMenu(mw.menu.GetMenu(s))
+		pres := mw.app.Preferences().StringWithFallback(s+prefsSelectedPreset, s+" Dash")
+		log.Println(pres)
+		mw.selects.presetSelect.SetSelected(pres)
+	})
+
+	mw.selects.presetSelect = widget.NewSelect(append([]string{"Select preset"}, presets.Names()...), func(presetName string) {
+		if presetName == "Select preset" {
 			return
 		}
-		preset, err := presets.Get(s)
+		preset, err := presets.Get(presetName)
 		if err != nil {
 			mw.Error(err)
 			return
 		}
 		mw.symbolList.LoadSymbols(preset...)
 		mw.SyncSymbols()
-		mw.app.Preferences().SetString(prefsSelectedPreset, s)
+		ecu := mw.app.Preferences().String(prefsSelectedECU)
+		mw.app.Preferences().SetString(ecu+prefsSelectedPreset, presetName)
 	})
 	mw.selects.presetSelect.Alignment = fyne.TextAlignLeading
 	mw.selects.presetSelect.PlaceHolder = "Select preset"
-
-	mw.selects.ecuSelect = widget.NewSelect([]string{"T5", "T7", "T8"}, func(s string) {
-		mw.app.Preferences().SetString(prefsSelectedECU, s)
-		idx := symbol.ECUTypeFromString(s)
-		ebus.Publish(ebus.TOPIC_ECU, float64(idx))
-		mw.SetMainMenu(mw.menu.GetMenu(s))
-	})
 }
