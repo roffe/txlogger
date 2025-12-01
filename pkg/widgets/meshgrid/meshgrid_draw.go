737ad4645367dca41f7e5f97bf0b59b1281e16ff
Roffe <joakim@roffe.nu>
2025-12-01 18:39:55 +0100

    remove unused

--- pkg/widgets/meshgrid/meshgrid_draw.go
+++ pkg/widgets/meshgrid/meshgrid_draw.go
@@ -365,17 +365,3 @@ func abs(v int) int {
 	}
 	return v
 }
-
-func min(a, b int) int {
-	if a < b {
-		return a
-	}
-	return b
-}
-
-func max(a, b int) int {
-	if a > b {
-		return a
-	}
-	return b
-}
