976e1dc1b7eaf49b01ab9f00c1554cd2f2941131
Roffe <joakim@roffe.nu>
2025-12-14 11:26:55 +0100

    update llvm

--- setup_build_env.ps1
+++ setup_build_env.ps1
@@ -1,7 +1,7 @@
 $temp_dir = ".\setup_temp"
 $canusb = "https://www.canusb.com/files/canusb_dll_driver.zip"
 $canlib = "https://pim.kvaser.com/var/assets/Product_Resources/canlib.exe"
-$llvm = "https://github.com/mstorsjo/llvm-mingw/releases/download/20251007/llvm-mingw-20251007-ucrt-x86_64.zip"
+$llvm = "https://github.com/mstorsjo/llvm-mingw/releases/download/20251202/llvm-mingw-20251202-ucrt-x86_64.zip"
 
 # create directory temp if not existing
 if (-not (Test-Path -Path "$temp_dir")) {
@@ -36,7 +36,7 @@ Expand-Archive -Path "$temp_dir\llvm-mingw.zip" -DestinationPath ".\" -Force
 
 # rename folder llvm-mingw-20251007-ucrt-x86_64 to llvm-mingw
 Write-Output "Renaming llvm-MinGW folder"
-Rename-Item -Path ".\llvm-mingw-20251007-ucrt-x86_64" -NewName "llvm-mingw"
+Rename-Item -Path ".\llvm-mingw-20251202-ucrt-x86_64" -NewName "llvm-mingw"
 
 Write-Output "Installing CANLIB"
 Start-Process -FilePath "$temp_dir\canlib.exe" -ArgumentList "/S" -Wait
